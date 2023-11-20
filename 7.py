e55e48ac0aaac4496e93f49092a80212f8e790d8
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-20 18:38:33 +0530

    first commit

---
+++ 7.py
@@ -0,0 +1,4 @@
+print((lambda x, y:x+y)(12,12))
+
+#Lambda Functions can either return a single expression or a list,  
+# tuple, set etc. So we need to change this code to return any of these to make it work.
